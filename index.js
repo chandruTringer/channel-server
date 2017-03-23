5cd341dbb30a1f7ca67d03ff120cd45a0d5dd2f5
vigneshwaransivasamy <vigneshwaransivasamy@gmail.com>
2017-03-23 09:50:45 +0530

    Local ran the app successfully

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // Setup basic express server
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('../..')(server);
-var port = process.env.PORT || 3000;
+
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const port = process.env.PORT || 3000;
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
