50ca2f4facb832393e4ebe145b2da5ee2f575308
vigneshwaransivasamy <vigneshwaransivasamy@gmail.com>
2017-03-24 14:17:01 +0530

    url updatation for siganlling

--- public/js/rtcLibrary.js
+++ public/js/rtcLibrary.js
@@ -487,7 +487,7 @@ Rtc.prototype.sendMessage = function(message) {
 Rtc.prototype._openChannel = function(channelToken) {
 
       var tempObj = this;
-      var socket = io();
+      var socket = io("https://webrtc-singaling-dot-icrdemo-1327.appspot.com:3000");
       socket.on('connect',function(){
         tempObj.onChannelOpened.call(tempObj);
         socket.emit('addUser',{
