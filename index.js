c6625a47a42fc190e018ea944bb1bc00586a9f36
vigneshwaransivasamy <vigneshwaransivasamy@gmail.com>
2017-03-28 17:13:19 +0530

    Channel Token addition

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 // Setup basic express server
 
 var express = require('express');
+var mongoose = require('mongoose');
+var User = require('./models/user.js');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+
+// Connecting to mongodb
+
+mongoose.connect('mongodb://localhost/users');
+
 var port = process.env.PORT || 3000;
 
 server.listen(port, function () {
@@ -13,27 +20,33 @@ server.listen(port, function () {
 // Routing
 app.use(express.static(__dirname + '/public'));
 
-
-var usersOnBoard = {};
-
-io.on('connection', function (socket) {
-  socket.on('addUser', function(data) {
-    usersOnBoard[data.userId] = data.socketId;
-    console.log(usersOnBoard);
-  });
-  socket.on('disconnect', function(data) {
-    console.log(usersOnBoard[data.userId]);
-    delete usersOnBoard[data.userId];
-  });
-  socket.on('sendMessage', function(data){
-    console.log(data);
-    var sendTo = usersOnBoard[data.sendTo];
-    var message = data.message;
-    if(sendTo){
-      console.log(sendTo);
-      socket.broadcast.to(sendTo).emit('message', message);
-    } else {
-      console.log("Unknow user id");
-    }
-  });
+app.get('/api/user/create/:_id', ( request, response ) => {
+  var id = request.params._id;
+  console.log(id);
+  User.addUser({userId : id}, (err, successResponse) => {
+    console.log(err);
+		if(err){
+			throw err;
+		}
+		response.json(successResponse);
+	});
 });
+
+// io.on('connection', function (socket) {
+//   socket.on('addUser', function(data) {
+//     usersOnBoard[data.userId] = data.socketId;
+//   });
+//   socket.on('disconnect', function(data) {
+//     delete usersOnBoard[data.userId];
+//   });
+//   socket.on('sendMessage', function(data){
+//     var sendTo = usersOnBoard[data.sendTo];
+//     var message = data.message;
+//     if(sendTo){
+//       console.log(sendTo);
+//       socket.broadcast.to(sendTo).emit('message', message);
+//     } else {
+//       console.log("Unknow user id");
+//     }
+//   });
+// });
