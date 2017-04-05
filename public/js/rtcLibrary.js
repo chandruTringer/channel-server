be14b6dcf9ae3024900b09f544882caff54f50c7
vigneshwaransivasamy <vigneshwaransivasamy@gmail.com>
2017-04-05 18:35:45 +0530

    Reconnection flow removal

--- public/js/rtcLibrary.js
+++ public/js/rtcLibrary.js
@@ -591,9 +591,6 @@ Rtc.prototype.afterCallAccepted = function(tempMsg){
         tempObj.trace("Client", "Message", ("User in room: " + tempMsg.userId));
         tempObj.createPeerConnection(tempMsg, "offer");
         if(!currentUser.isBusyWith || isReconnection) {
-              if(isReconnection){
-                currentUser.connections = {};
-              }
               tempObj.dataChannelCalls(currentUser.connections[tempMsg.userId].peerConnection, tempMsg.userId);
               tempObj.doCallTo(tempMsg.userId);
               currentUser.connections[tempMsg.userId].makeCall = true;
